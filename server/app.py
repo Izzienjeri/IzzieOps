f052377c67274cfc85d787891f5bd2d8df9e744a
izzie <izzienjeri12@gmail.com>
2024-11-02 05:27:20 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_cors import CORS  # Import CORS
 from config import Config
 from flask_migrate import Migrate
-from extensions import db, mail  # Import mail
+from extensions import db, mail
 from serializer import serializer_bp
 from routes.onboarding import onboarding_bp
 from routes.attendance import attendance_bp
@@ -12,16 +13,17 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    
+
+    CORS(app)  
+
     # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
-    mail.init_app(app)  
+    mail.init_app(app)
 
     app.register_blueprint(serializer_bp)
     app.register_blueprint(onboarding_bp)
     app.register_blueprint(attendance_bp)
-
 
     # Create tables if not exist
     with app.app_context():
